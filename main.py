f60dfbae93cb26f6d01302c61d947aa3bf1b5101
Andrela2025 <andreapgiraldoo@gmail.com>
2025-05-18 12:34:40 -0500

    Creamos ruta para obtener lista de todas las peliculas disponibles

--- main.py
+++ main.py
@@ -22,4 +22,45 @@ nltk.data.path.append(r'C:\Users\andre\AppData\Roaming\nltk_data')
 
 def load_movies():
     # leemos el archivo que contiene informacion de peliculas y seleccionamos las columnas mas importantes
-    df = pd.read_csv (r"Dataset/netfli_titles.csv")
+    df = pd.read_csv (r"Dataset/netfli_titles.csv")[['show_id', 'title', 'realese_year', 'listed_in', 'rating', 'description']]
+    
+    # Renombramos las columnas para que sean mas faciles de entender
+    
+    df.columns = ['id', 'title', 'year', 'category', 'rating', 'overview'] 
+    
+    # Llenamos los espcacios basiosvacios con texto vacio y convertimos los datos en una lista de diccionarios
+    return df.fillna('').to_dict(orient='records')
+
+# Cargamos las peliculas al iniciar la API para no leer el archivo cada vez que alguien pregunte por ellas
+movies_list = load_movies()
+
+# Funcion para encontrar sinonomos de una palabra
+
+def get_synonyms(word):
+    # Usamos Wornet para obtener distintas palabras que signifiquen lo mismo.
+    return{lema.name().lower() for syn in wordnet.synset(word) for lema in syn.lemas()}
+
+# Creamos la aplicación FastAPI, que será el motor de nuesta API
+# Esto inicializa la API con un nombre y una versión
+app = FastAPI(title="Mi aplicacion de peliculas", versión= "1.0.0")
+
+# Ruta de inicio: Cuando alguien entra a la API sin especificar nada, verá un mensaje de bienvenida.
+
+@app.get('/', tags=['home'])
+def home():
+# Cuando entremos en el navegador a HTTP://127.0.0.1:8000/ veremos el mensaje de bienvenida
+    return HTMLResponse('<h1>Bienvenido a la API de Películas</h1>')
+
+# Obteniendo lista de películas 
+# Creamos una ruta para obtener todas las películas
+
+# Ruta para obtener todas las películas disponibles
+
+
+@app.get('movies', tags=['Movies'])
+def get_movies():
+    # Si hay películas, las enviamos, si no, mostramo un error
+    return movies_list or HTTPException(status_code=500, detail="No hay datos de películas disponibles")
+
+# Ruta para obtener una películas especifica segun su ID
+
